20b8927d19f171f6dd38b714c8e60ce4e1135278
Juliana Amoasei <juliana.amoasei@gmail.com>
2022-02-01 14:53:39 -0300

    vídeos 2.3 e 2.4

--- objetoLiteral.js
+++ objetoLiteral.js
@@ -2,26 +2,25 @@ const user = {
   nome: "Juliana",
   email: "j@j.com",
   nascimento: "2021/01/01",
-  role: "admin",
+  role: "estudante",
   ativo: true,
   exibirInfos: function() {
     console.log(this.nome, this.email)
   }
 }
 
-// user.exibirInfos()
-// const exibir = user.exibirInfos
-// exibir()
-
-const exibir = function() {
-  console.log(this.nome)
+const admin = {
+  nome: "Mariana",
+  email: "m@m.com",
+  role: "admin",
+  criarCurso() {
+    console.log('curso criado!')
+  }
 }
 
-const exibirNome = exibir.bind(user) 
-exibirNome()
-exibir()
-
-
+Object.setPrototypeOf(admin, user)
+admin.criarCurso()
+admin.exibirInfos()
 
 
 

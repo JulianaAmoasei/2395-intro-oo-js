0f72aa2c64ff9f8964fcb1c623ff01ebdc1176e1
Juliana Amoasei <juliana.amoasei@gmail.com>
2022-02-02 11:51:20 -0300

    video 3.2

---
+++ Admin.js
@@ -0,0 +1,11 @@
+import User from "./User.js";
+
+class Admin extends User {
+  constructor(nome, email, nascimento, role = 'admin', ativo = true) {
+    super(nome, email, nascimento, role, ativo)
+  }
+}
+
+const novoAdmin = new Admin('Rodrigo', 'r@r.com', '2021-01-01')
+console.log(novoAdmin)
+console.log(novoAdmin.exibirInfos())
--- User.js
+++ User.js
@@ -1,4 +1,4 @@
-class User {
+export default class User {
   constructor(nome, email, nascimento, role, ativo = true) {
     this.nome = nome
     this.email = email
@@ -6,14 +6,8 @@ class User {
     this.role = role || 'estudante'
     this.ativo = ativo
   }
-
+ 
   exibirInfos() {
     return `${this.nome}, ${this.email}`
   }
 }
-
-const novoUser = new User('Juliana', 'j@j.com', '2021-01-01')
-console.log(novoUser)
-console.log(novoUser.exibirInfos())
-
-console.log(User.prototype.isPrototypeOf(novoUser)) //true
